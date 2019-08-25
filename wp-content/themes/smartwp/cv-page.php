b15799107f99fe3d32ad52f7813793e34677ec61
skgandikota <gsaikoushik@yahoo.com>
2019-08-25 08:52:44 +0530

    modified CV Template

--- wp-content/themes/smartwp/cv-page.php
+++ wp-content/themes/smartwp/cv-page.php
@@ -10,10 +10,9 @@
     <meta name="description" content="Creative CV is a HTML resume template for professionals. Built with Bootstrap 4, Now UI Kit and FontAwesome, this modern and responsive design template is perfect to showcase your portfolio, skils and experience."/>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href="<?php echo get_template_directory_uri(); ?>css/aos.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri(); ?>css/main.css" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="styles/main.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/css/aos.css" rel="stylesheet">
+	<link href="<?php echo get_template_directory_uri(); ?>/styles/main.css" rel="stylesheet">
+
   </head>
   <body id="top">
     <header>
