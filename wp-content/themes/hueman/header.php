639a74729aabb6c339f6641f0ae6a725940e4dd3
skgandikota <gsaikoushik@yahoo.com>
2019-08-25 09:22:30 +0530

    Added new theme

---
+++ wp-content/themes/hueman/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="profile" href="https://gmpg.org/xfn/11" />
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php
+//see https://github.com/presscustomizr/hueman/issues/784
+if ( function_exists( 'wp_body_open' ) ) {
+    wp_body_open();
+} else {
+    do_action( 'wp_body_open' );
+}
+?>
+<div id="wrapper">
+
+  <?php do_action('__before_header') ; ?>
+
+  <?php hu_get_template_part('parts/header-main'); ?>
+
+  <?php do_action('__after_header') ; ?>
+
+  <div class="container" id="page">
+    <div class="container-inner">
+      <?php do_action('__before_main') ; ?>
+      <div class="main">
+        <div class="main-inner group">
+          <?php do_action('__before_content') ; ?>
